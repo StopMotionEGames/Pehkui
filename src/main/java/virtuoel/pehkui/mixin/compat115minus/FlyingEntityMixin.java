2b0beca2b1695c180b8453f2a9de61d21acc460e
Ravi Widnef <163608714+StopMotionEGames@users.noreply.github.com>
2026-01-18 14:58:41 -0300

    FlyingEntityMixin fix (experimental)
    Target class is now LivingEntity
    Target method is now updateLimbs(F)V

--- src/main/java/virtuoel/pehkui/mixin/compat115minus/FlyingEntityMixin.java
+++ src/main/java/virtuoel/pehkui/mixin/compat115minus/FlyingEntityMixin.java
@@ -7,15 +7,16 @@ import org.spongepowered.asm.mixin.injection.At;
 import com.llamalad7.mixinextras.injector.ModifyExpressionValue;
 
 import net.minecraft.entity.Entity;
-import net.minecraft.entity.mob.FlyingEntity;
+import net.minecraft.entity.LivingEntity;
 import virtuoel.pehkui.util.MixinConstants;
 import virtuoel.pehkui.util.ScaleUtils;
 
-@Mixin(FlyingEntity.class)
+@Mixin(LivingEntity.class)
 public class FlyingEntityMixin
 {
-	@Dynamic
-	@ModifyExpressionValue(method = MixinConstants.TRAVEL, at = @At(value = "CONSTANT", args = "floatValue=4.0F"))
+
+	// todo: see if nothing breaks
+	@ModifyExpressionValue(method = "updateLimbs(F)V", at = @At(value = "CONSTANT", args = "floatValue=4.0F"))
 	private float pehkui$travel$limbDistance(float value)
 	{
 		return ScaleUtils.modifyLimbDistance(value, (Entity) (Object) this);
