db6df571f2eb6ae79080152d60dccab6ae1a0efd
Ravi Widnef <163608714+StopMotionEGames@users.noreply.github.com>
2026-01-19 00:32:03 -0300

    fix method signature

--- src/main/java/virtuoel/pehkui/mixin/PortalBlockMixin.java
+++ src/main/java/virtuoel/pehkui/mixin/PortalBlockMixin.java
@@ -18,7 +18,7 @@ import virtuoel.pehkui.util.PehkuiBlockStateExtensions;
 public abstract class PortalBlockMixin
 {
 	@Inject(at = @At("HEAD"), method = "onEntityCollision", cancellable = true)
-	private void pehkui$onEntityCollision(BlockState state, World world, BlockPos pos, Entity entity, EntityCollisionHandler handler, CallbackInfo ci)
+	private void pehkui$onEntityCollision(BlockState state, World world, BlockPos pos, Entity entity, EntityCollisionHandler handler, boolean bl, CallbackInfo ci)
 	{
 		if (PehkuiConfig.COMMON.accurateNetherPortals.get())
 		{
