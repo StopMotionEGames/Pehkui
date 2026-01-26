10c936796b6a23732bd48c569fa4d5510fe40c2e
Ravi Widnef <163608714+StopMotionEGames@users.noreply.github.com>
2026-01-26 13:52:21 -0300

    Fix: Elytra fall damage check
    Landing on the ground with elytra no longer deals massive damage when the scaled down, even when landing softly.

--- src/main/java/virtuoel/pehkui/mixin/world/entity/LivingEntityMixin.java
+++ src/main/java/virtuoel/pehkui/mixin/world/entity/LivingEntityMixin.java
@@ -1,17 +1,5 @@
 package virtuoel.pehkui.mixin.world.entity;
 
-import com.llamalad7.mixinextras.injector.ModifyExpressionValue;
-import net.minecraft.server.level.ServerLevel;
-import net.minecraft.world.entity.EntityDimensions;
-import org.spongepowered.asm.mixin.Mixin;
-import org.spongepowered.asm.mixin.Unique;
-import org.spongepowered.asm.mixin.injection.At;
-import org.spongepowered.asm.mixin.injection.At.Shift;
-import org.spongepowered.asm.mixin.injection.Inject;
-import org.spongepowered.asm.mixin.injection.ModifyArg;
-import org.spongepowered.asm.mixin.injection.ModifyVariable;
-import org.spongepowered.asm.mixin.injection.callback.CallbackInfo;
-
 import com.llamalad7.mixinextras.injector.ModifyReturnValue;
 import com.llamalad7.mixinextras.injector.wrapoperation.Operation;
 import com.llamalad7.mixinextras.injector.wrapoperation.WrapOperation;
@@ -20,11 +8,20 @@ import net.minecraft.core.BlockPos;
 import net.minecraft.util.Mth;
 import net.minecraft.world.damagesource.DamageSource;
 import net.minecraft.world.entity.Entity;
+import net.minecraft.world.entity.EntityDimensions;
 import net.minecraft.world.entity.LivingEntity;
 import net.minecraft.world.level.Level;
 import net.minecraft.world.level.block.ScaffoldingBlock;
 import net.minecraft.world.phys.AABB;
 import net.minecraft.world.phys.Vec3;
+import org.spongepowered.asm.mixin.Mixin;
+import org.spongepowered.asm.mixin.Unique;
+import org.spongepowered.asm.mixin.injection.At;
+import org.spongepowered.asm.mixin.injection.At.Shift;
+import org.spongepowered.asm.mixin.injection.Inject;
+import org.spongepowered.asm.mixin.injection.ModifyArg;
+import org.spongepowered.asm.mixin.injection.ModifyVariable;
+import org.spongepowered.asm.mixin.injection.callback.CallbackInfo;
 import virtuoel.pehkui.util.MulticonnectCompatibility;
 import virtuoel.pehkui.util.PehkuiBlockStateExtensions;
 import virtuoel.pehkui.util.ScaleUtils;
@@ -91,9 +88,8 @@ public abstract class LivingEntityMixin extends EntityMixin {
 		return original;
 	}
 
-	@ModifyArg(method = "hurtServer", at = @At(value = "INVOKE", target = "Lnet/minecraft/world/entity/LivingEntity;knockback(DDD)V"),index = 0)
-	private double pehkui$damage$knockback(double strength, @Local(argsOnly = true) DamageSource source)
-	{
+	@ModifyArg(method = "hurtServer", at = @At(value = "INVOKE", target = "Lnet/minecraft/world/entity/LivingEntity;knockback(DDD)V"), index = 0)
+	private double pehkui$damage$knockback(double strength, @Local(argsOnly = true) DamageSource source) {
 		final float scale = ScaleUtils.getKnockbackScale(source.getEntity());
 
 		return scale != 1.0F ? scale * strength : strength;
@@ -230,8 +226,7 @@ public abstract class LivingEntityMixin extends EntityMixin {
 
 	// step height
 	@ModifyReturnValue(method = "maxUpStep()F", at = @At("RETURN"))
-	private float pehkui$getStepHeight(float original)
-	{
+	private float pehkui$getStepHeight(float original) {
 		final float scale = ScaleUtils.getStepHeightScale((Entity) (Object) this);
 
 		return scale != 1.0F ? original * scale : original;
