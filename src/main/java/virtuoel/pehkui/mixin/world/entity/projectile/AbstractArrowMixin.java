a378c8f0ab14a643c5a7b267be09d362b51370b1
Ravi Widnef <163608714+StopMotionEGames@users.noreply.github.com>
2026-01-25 23:38:54 -0300

    fix projectiles wrong scaled velocity and wrong head alignment

--- src/main/java/virtuoel/pehkui/mixin/world/entity/projectile/AbstractArrowMixin.java
+++ src/main/java/virtuoel/pehkui/mixin/world/entity/projectile/AbstractArrowMixin.java
@@ -1,15 +1,12 @@
 package virtuoel.pehkui.mixin.world.entity.projectile;
 
+import com.llamalad7.mixinextras.injector.ModifyReturnValue;
 import com.llamalad7.mixinextras.sugar.Local;
 import net.minecraft.world.entity.Entity;
-import net.minecraft.world.entity.EntityType;
 import net.minecraft.world.entity.LivingEntity;
-import net.minecraft.world.entity.projectile.AbstractArrow;
 import net.minecraft.world.entity.projectile.Projectile;
-import net.minecraft.world.item.ItemStack;
-import net.minecraft.world.level.Level;
+import net.minecraft.world.entity.projectile.AbstractArrow;
 import net.minecraft.world.phys.AABB;
-import net.minecraft.world.phys.Vec3;
 import org.jetbrains.annotations.Nullable;
 import org.spongepowered.asm.mixin.Mixin;
 import org.spongepowered.asm.mixin.injection.At;
@@ -19,21 +16,16 @@ import org.spongepowered.asm.mixin.injection.ModifyVariable;
 import org.spongepowered.asm.mixin.injection.callback.CallbackInfo;
 import virtuoel.pehkui.util.ScaleUtils;
 
-import java.util.function.Predicate;
-
 @Mixin(AbstractArrow.class)
 public abstract class AbstractArrowMixin {
-	@Inject(at = @At("RETURN"), method = "<init>(Lnet/minecraft/world/entity/EntityType;Lnet/minecraft/world/entity/LivingEntity;Lnet/minecraft/world/level/Level;Lnet/minecraft/world/item/ItemStack;Lnet/minecraft/world/item/ItemStack;)V")
-	private void pehkui$construct(EntityType<? extends Projectile> type, LivingEntity owner, Level world, ItemStack stack, ItemStack shotFrom, CallbackInfo info) {
-		final float scale = ScaleUtils.getEyeHeightScale(owner);
+	@ModifyArg(at = @At(value = "INVOKE", target = "Lnet/minecraft/world/entity/projectile/AbstractArrow;<init>(Lnet/minecraft/world/entity/EntityType;DDDLnet/minecraft/world/level/Level;Lnet/minecraft/world/item/ItemStack;Lnet/minecraft/world/item/ItemStack;)V"),
+		method = "<init>(Lnet/minecraft/world/entity/EntityType;Lnet/minecraft/world/entity/LivingEntity;Lnet/minecraft/world/level/Level;Lnet/minecraft/world/item/ItemStack;Lnet/minecraft/world/item/ItemStack;)V",
+		index = 2
+	)
+	private static double pehkui$construct$yOffset(double d, @Local(argsOnly = true) LivingEntity owner) {
+		float scale = ScaleUtils.getEyeHeightScale(owner);
 
-		if (scale != 1.0F) {
-			final Entity self = ((Entity) (Object) this);
-
-			final Vec3 pos = self.position();
-
-			self.setPos(pos.x, pos.y + ((1.0F - scale) * 0.1D), pos.z);
-		}
+		return owner.getEyeY() - (0.1D * scale);
 	}
 
 	@Inject(at = @At("HEAD"), method = "setOwner")
@@ -43,8 +35,15 @@ public abstract class AbstractArrowMixin {
 		}
 	}
 
+	@ModifyArg(method = "shouldFall", at = @At(value = "INVOKE", target = "Lnet/minecraft/world/phys/AABB;inflate(D)Lnet/minecraft/world/phys/AABB;"))
+	private double pehkui$shouldFall$inflate(double d){
+		final float scale = ScaleUtils.getModelHeightScale((Entity) (Object) this);
+
+		return scale != 1.0F ? d * scale : d;
+	}
+
 	@ModifyArg(method = "findHitEntity", index = 4, at = @At(value = "INVOKE", target = "Lnet/minecraft/world/entity/projectile/ProjectileUtil;getEntityHitResult(Lnet/minecraft/world/level/Level;Lnet/minecraft/world/entity/projectile/Projectile;Lnet/minecraft/world/phys/Vec3;Lnet/minecraft/world/phys/Vec3;Lnet/minecraft/world/phys/AABB;Ljava/util/function/Predicate;)Lnet/minecraft/world/phys/EntityHitResult;"))
-	private AABB pehkui$getEntityCollision$expand(AABB box) {
+	private AABB pehkui$findHitEntity$inflate(AABB box) {
 		Projectile projectile = (Projectile) (Object) this;
 
 		final float width = ScaleUtils.getBoundingBoxWidthScale(projectile);
@@ -58,9 +57,22 @@ public abstract class AbstractArrowMixin {
 	}
 
 	@ModifyVariable(method = "onHitEntity", at = @At(value = "STORE"))
-	private float pehkui$onEntityHit(float value) {
+	private float pehkui$onHitEntity(float value) {
 		final float scale = ScaleUtils.getMotionScale((Entity) (Object) this);
 
 		return scale != 1.0F ? ScaleUtils.divideClamped(value, scale) : value;
+	}
+
+	@ModifyArg(method = "onHitBlock", at = @At(value = "INVOKE", target = "Lnet/minecraft/world/phys/Vec3;scale(D)Lnet/minecraft/world/phys/Vec3;"))
+	private double pehkui$onHitBlock(double value) {
+		final float scale = ScaleUtils.getModelHeightScale((Entity) (Object) this);
+		return scale != 1.0F ? value * scale : value;
+	}
+
+	@ModifyReturnValue(method = "getDefaultGravity", at = @At(value = "RETURN"))
+	private double pehkui$getDefaultGravity(double gravity) {
+		float scale = ScaleUtils.getMotionScale((Entity) (Object) this);
+
+		return scale != 1.0F ? gravity * scale : gravity;
 	}
 }
